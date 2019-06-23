e58de04aced977d7864911bccf819e92dbcb882a
alizain <ali.javed@tkxel.com>
2019-06-23 06:08:04 +0500

    intial commmit after changing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [upload, setUpload] = useState(false);
+  const [url, Uploadurl] = useState();
+  function videoUpload(e){
+    Uploadurl(e.target.value)
+
+    console.log('data',e.target.value);
+    setUpload(true);
+    }
+    function Onback()
+    {
+      setUpload(false);
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     { upload===false ?<div><img src={logo} className="App-logo" alt="logo" />
+        <input type="file" accept="video/mp4,video/x-m4v,video/*" onChange={videoUpload}/>
+               </div>: <div> 
+                
+          <div className="smartphone">
+              <div className="content">
+              <video width="100%" height="100%" controls>
+  <source src={url} type="video/mp4" />
+</video>
+                             </div>
+                               </div>
+                               <button onClick={Onback}>back</button>                               </div>
+          }    
       </header>
     </div>
   );
